4c7392a34d9ceef6efb6abbac326817f79a1d258
bmsohwinc <bharasou58@gmail.com>
2026-05-24 16:31:31 -0700

    minor comment syntax fix

--- 5gc/upf_u/upf_u.c
+++ 5gc/upf_u/upf_u.c
@@ -748,7 +748,7 @@ GetPdrByTeid(struct rte_mbuf *pkt, const gtp_parse_result_t *gtp_info) {
         uint16_t udp_len = rte_be_to_cpu_16(innerU->dgram_len);
         uint16_t payload_len = udp_len > sizeof(*innerU) ? udp_len - sizeof(*innerU) : 0;
         // log_udp_seq_sample("UL inner before decap", inner4, innerU, payload,
-                           payload_len, gtp_info->teid, gtp_info->qfi, pkt->port);
+        //                    payload_len, gtp_info->teid, gtp_info->qfi, pkt->port);
     }
     // log_classifier_key("UL", &key);
     const UPDK_PDR *pdr = UpfClassifyGetPdrPtr(&key);
